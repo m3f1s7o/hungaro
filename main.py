5ad87c86f917b3450c9b809dbe735eaaf043cb92
james <jimmy.za@protonmail.com>
2022-12-01 10:54:31 -0600

    main y steps separados

--- main.py
+++ main.py
@@ -1,207 +1,84 @@
 #!/bin/python3
-
 from tabulate import tabulate
-
-def obtnrTabla():
-    dim = int(input("Dimensiones de la tabla(NxN) N: "));
-
-    matriz = []
-    tmp = []
-    
-    for i in range(dim):
-        row = input(f"Valores de la fila {i} (1,2,..,n): ")
-        
-        matriz.append(row.split(","))
-
-    return matriz
-
-def defTabla():
-    return[[230,200,210,240],
-           [190,210,200,200],
-           [200,180,240,220],
-           [220,180,210,230]]
-    """
-    return[[3,5,3,3],
-           [5,14,10,10],
-           [12,6,19,17],
-           [2,17,10,12]]
-    """
-
-
-""" seleccionar el valor mas pequeño de cada columna y restarlo con la columna """
-def paso1(matriz):
-
-    min_col = []
-    for i in range(len(matriz)):
-       min_col.append(min([c[i] for c in matriz]))  # obtiene el val minimo de cada columna 
-    
-    matriz_a = []
-    for i in range(len(matriz)):  # resta el minimo de cada columna con toda la columna
-        fila = [int(f) - int(m) for f, m in zip(matriz[i], min_col)] 
-
-        matriz_a.append(fila)
-
-    return matriz_a
-    
-""" seleccionar el valor mas pequeño de cada fila y restarlo con la fila """
-def paso2(matriz_a):
-    min_fil = []
-
-    for fila in matriz_a:
-        min_fil.append(min(fila))  # obtiene el min valor de cada fila
-    
-    matriz_b = []
-    for i in range(len(matriz_a)):  # resta el minimo de cada fila con toda la fila
-        fila = [int(f) - min_fil[i] for f in matriz_a[i]]
-
-        matriz_b.append(fila)
-
-    return matriz_b
-
-""" realiza todas las asignaciones posibles """
-def paso3(matriz_a, recursivo):
-    exito = False
-
-    for i in range(len(matriz_a)):
-        for j in range(len(matriz_a)):
-            if matriz_a[i][j] == 0:
-                
-                if posibleAsg(matriz_a, i, j):
-                    matriz_a[i][j] = "x";
-                   
-                    if recursivo:
-                        paso3(matriz_a, recursivo)
-                    
-                        # si hay igual de asignaciones que el tamaño de la tabla, termina
-                        if sum([x.count("x") for x in matriz_a]) == len(matriz_a):
-                            exito = True
-                            break
-
-                        matriz_a[i][j] = 0
-
-    return matriz_a, exito
-
-
-""" identifica si la casilla es apta para asignacion """
-def posibleAsg(matriz_b, x, y): 
-    for i in range(len(matriz_b)):
-        if matriz_b[x][i] == "x": 
-            return False
-        
-        if matriz_b[i][y] == "x":
-            return False
-
-    return True
-    
-""" marca filas y columnas """
-def paso4(matriz_a):
-    matriz_a, e = paso3(matriz_a, False)
-    
-    m_a = []  # filas que no tienen asignaciones
-    for i in range(len(matriz_a)):
-        if matriz_a[i].count("x") == 0:
-            m_a.append(i)
-
-    m_b = []  # columnas con 0 en las filas de la marca a
-    for i in range(len(matriz_a)):
-        for j in range(len(m_a)):
-            if matriz_a[m_a[j]][i] == 0:
-                m_b.append(i)
-    
-    m_c = []  # filas con asignacion de las col de la marca b
-    for i in range(len(matriz_a)):
-        for j in range(len(m_b)):
-            if matriz_a[i][m_b[j]] == "x":
-                m_c.append(i)
-
-    # linea en las casillas de filas que no estan marcadas y columnas marcadas
-    matriz_b = []
-    for i in range(len(matriz_a)):
-        fila = []
-        for j in range(len(matriz_a)):
-            if (i in m_a or i in m_c) and j not in m_b:
-                fila.append(matriz_a[i][j])
-            
-            else:
-                fila.append("-")
-        
-        matriz_b.append(fila)
-
-    return matriz_b, m_b
-
-""" suma el menor val a las casillas sin - y lo suma a los demas """
-def paso5(matriz_a, matriz_c, col_m):
-    matriz_d = []
-
-    min_mb = []
-
-    for i in range(len(matriz_a)):  # obtiene el minimo excluyendo "-"
-        for j in range(len(matriz_a)):
-            if matriz_c[i][j] != "-":
-                min_mb.append(matriz_c[i][j])
-
-    min_mb = min(min_mb)
-
-    for i in range(len(matriz_a)):
-        fila = []
-        for j in range(len(matriz_a)):
-            if matriz_c[i][j] != "-":  # casillas no rayadas - val min
-                if matriz_a[i][j] == "x":
-                    fila.append(0)
-
-                else:
-                    fila.append(matriz_a[i][j] - min_mb)
-
-            else:
-                if matriz_a[i][j] == "x": 
-                    fila.append(0)
-
-                elif j in col_m and matriz_a[i][j] != 0: # col_m + val min
-                    fila.append(matriz_a[i][j] + min_mb)
-
-                else:
-                    fila.append(matriz_a[i][j])
-
-        
-        matriz_d.append(fila)
-
-    return matriz_d
-
-
+from steps import *
 
 def main():
-   #matriz = obtnrTabla()
-   matriz = defTabla()
-   titulos = ["C" + str(i) for i in range(len(matriz))]
 
-   print(tabulate(matriz, headers=titulos, tablefmt="psql"))
+    matriz = obtnrTabla()
+   # matriz = defTabla()
+    titulos = ["C" + str(i) for i in range(len(matriz))]
+    print_t = lambda matriz_n: print(tabulate(matriz_n, headers=["C" + str(i) for i in range(len(matriz))], tablefmt="psql"))
 
-   matriz_a = paso1(matriz)
-   print(tabulate(matriz_a, headers=titulos, tablefmt="psql"))
 
-   matriz_a = paso2(matriz_a)
-   print("Matriz a")
-   print(tabulate(matriz_a, headers=titulos, tablefmt="psql"))
+    # tabla original
+    #print(tabulate(matriz, headers=titulos, tablefmt="psql"))
+    print_t(matriz)
 
-   matriz_b, exito = paso3(matriz_a, True)
+    # tabla original - min c columna
+    matriz_a = paso1(matriz)
+    print("Paso 1")
+    print_t(matriz_a)
 
-   if not exito:
-       matriz_c, col_m = paso4(matriz_b)
-       print(tabulate(matriz_c, headers=titulos, tablefmt="psql"))
+    # tabla - min c fila
+    matriz_b = paso2(matriz_a)
+    print("Paso 2")
+    print_t(matriz_b)
 
-       matriz_d = paso5(matriz_a, matriz_c, col_m)
-       print(tabulate(matriz_d, headers=titulos, tablefmt="psql"))
+    # tabla asignaciones
+    matriz_c, exito = paso3(matriz_b, len(matriz))
+    print("Paso 3")
+    print_t(matriz_c)
+
+    # termina con la solucion del problema o cuando se agoten los intentos
+    intentos = 0
+    while(not exito and intentos <= 10):
+        if intentos != 0:
+            print(f"\n\nIntento {intentos}")
+
+        # tabla rayada
+        matriz_d, col_m = paso4(matriz_c)
+        print("Paso 4")
+        print_t(matriz_d)
+
+        # menor de las no rayadas
+        matriz_e = paso5(matriz_b, matriz_d, col_m)
+        print("Paso 5")
+        print_t(matriz_e)
        
-       matriz_b, exito = paso3(matriz_d, True)
+        # asignaciones
+        matriz_c, exito = paso3(matriz_e, len(matriz))
+        print("Paso 3")
+        print_t(matriz_c)
+        
+        intentos += 1
+
+    if exito:
+        print("Solucion")
+        print_t(matriz_c)
+
+    else:
+        print("No se pudo dar solucion al problema")
+
+"""
+    if not exito:
+        # tabla rayada
+        matriz_d, col_m = paso4(matriz_c)
+        print("Paso 4")
+        print(tabulate(matriz_d, headers=titulos, tablefmt="psql"))
+
+        # menor de las no rayadas
+        matriz_e = paso5(matriz_b, matriz_d, col_m)
+        print("Paso 5")
+        print(tabulate(matriz_e, headers=titulos, tablefmt="psql"))
+
+        # asignaciones
+        matriz_f, exito = paso3(matriz_e, True)
+        print("Paso 3")
+        print(tabulate(matriz_f, headers=titulos, tablefmt="psql"))
+
+    else:
        print(tabulate(matriz_b, headers=titulos, tablefmt="psql"))
-
-   else:
-       print(tabulate(matriz_a, headers=titulos, tablefmt="psql"))
-
-
-
-
-
+"""
 
 if __name__ == "__main__":
     main()
---
+++ steps.py
@@ -0,0 +1,166 @@
+#!/bin/python3
+
+def obtnrTabla():
+    dim = int(input("Dimensiones de la tabla(NxN) N: "));
+
+    matriz = []
+    tmp = []
+    
+    for i in range(dim):
+        row = input(f"Valores de la fila {i} (1,2,..,n): ")
+        
+        # separa los numeros de cada fila y los pone en una lista como enteros
+        matriz.append([int(x) for x in row.split(",")])
+
+    return matriz
+
+def defTabla():
+#    return[[230,200,210,240],
+#           [190,210,200,200],
+#           [200,180,240,220],
+#           [220,180,210,230]]
+    return[[3,5,3,3],
+           [5,14,10,10],
+           [12,6,19,17],
+           [2,17,10,12]]
+
+""" seleccionar el valor mas pequeño de cada columna y restarlo con la columna """
+def paso1(matriz):
+
+    min_col = []
+    for i in range(len(matriz)):
+       min_col.append(min([c[i] for c in matriz]))  # obtiene el val minimo de cada columna 
+    
+    matriz_a = []
+    for i in range(len(matriz)):  # resta el minimo de cada columna con toda la columna
+        fila = [int(f) - int(m) for f, m in zip(matriz[i], min_col)] 
+
+        matriz_a.append(fila)
+
+    return matriz_a
+    
+""" seleccionar el valor mas pequeño de cada fila y restarlo con la fila """
+def paso2(matriz_a):
+    min_fil = []
+
+    for fila in matriz_a:
+        min_fil.append(min(fila))  # obtiene el min valor de cada fila
+    
+    matriz_b = []
+    for i in range(len(matriz_a)):  # resta el minimo de cada fila con toda la fila
+        fila = [int(f) - min_fil[i] for f in matriz_a[i]]
+
+        matriz_b.append(fila)
+
+    return matriz_b
+
+""" realiza todas las asignaciones posibles """
+def paso3(matriz, meta):
+    exito = False
+
+    for i in range(len(matriz)):
+        for j in range(len(matriz)):
+            if matriz[i][j] == 0:
+                
+                if posibleAsg(matriz, i, j):
+                    matriz[i][j] = "x";
+                   
+                    if not exito:
+                        paso3(matriz, meta)
+                    
+                        # si hay igual de asignaciones que el tamaño de la tabla, termina porque se encontro la solucion
+                        if sum([x.count("x") for x in matriz]) == meta:
+                            exito = True
+                            break
+
+                        # reset a la casilla
+                        matriz[i][j] = 0
+
+    return matriz, exito
+
+
+""" identifica si la casilla es apta para asignacion (si no hay otra casilla asignada en la misma fila y columna) """
+def posibleAsg(matriz_b, x, y): 
+    for i in range(len(matriz_b)):
+        if matriz_b[x][i] == "x": 
+            return False
+        
+        if matriz_b[i][y] == "x":
+            return False
+
+    return True
+    
+""" marca filas y columnas """
+def paso4(matriz_a):
+    matriz_a, e = paso3(matriz_a, len(matriz_a)-1)
+    
+    m_a = []  # filas que no tienen asignaciones
+    for i in range(len(matriz_a)):
+        if matriz_a[i].count("x") == 0:
+            m_a.append(i)
+
+    m_b = []  # columnas con 0 en las filas de la marca a
+    for i in range(len(matriz_a)):
+        for j in range(len(m_a)):
+            if matriz_a[m_a[j]][i] == 0:
+                m_b.append(i)
+    
+    m_c = []  # filas con asignacion de las col de la marca b
+    for i in range(len(matriz_a)):
+        for j in range(len(m_b)):
+            if matriz_a[i][m_b[j]] == "x":
+                m_c.append(i)
+
+    # linea en las casillas de filas que no estan marcadas y columnas marcadas
+    matriz_b = []
+    for i in range(len(matriz_a)):
+        fila = []
+        for j in range(len(matriz_a)):
+            if (i in m_a or i in m_c) and j not in m_b:
+                fila.append(matriz_a[i][j])
+            
+            else:
+                fila.append("-")
+        
+        matriz_b.append(fila)
+
+    return matriz_b, m_b
+
+""" suma el menor val a las casillas sin - y lo suma a los demas """
+def paso5(matriz_a, matriz_c, col_m):
+    matriz_d = []
+
+    min_mb = []
+
+    for i in range(len(matriz_a)):  # obtiene el minimo excluyendo "-"
+        for j in range(len(matriz_a)):
+            if matriz_c[i][j] != "-":
+                min_mb.append(matriz_c[i][j])
+
+    min_mb = min(min_mb)
+
+    for i in range(len(matriz_a)):
+        fila = []
+        for j in range(len(matriz_a)):
+            if matriz_c[i][j] != "-":  # casillas no rayadas - val min
+                if matriz_a[i][j] == "x":
+                    fila.append(0)
+
+                else:
+                    fila.append(matriz_a[i][j] - min_mb)
+
+            else:
+                if matriz_a[i][j] == "x": 
+                    fila.append(0)
+
+                elif j in col_m and matriz_a[i][j] != 0: # col_m + val min
+                    fila.append(matriz_a[i][j] + min_mb)
+
+                else:
+                    fila.append(matriz_a[i][j])
+
+        
+        matriz_d.append(fila)
+
+    return matriz_d
+
